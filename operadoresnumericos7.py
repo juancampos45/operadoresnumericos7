1d754906eb042dfc3e1fee92879f57173f6b9cfa
juancampos45 <55142610+juancampos45@users.noreply.github.com>
2019-09-12 10:41:04 -0400

    Add files via upload

---
+++ operadoresnumericos7.py
@@ -0,0 +1,22 @@
+# Proposito: Trabajar con los operadores aritmrtico
+input('Suponga que a=49 b=5 c=3, pulse cualquier tecla para realizar el siguiente programa: ')
+input('24/(c * 4), pulse cualquier tecla para obtener su resultado')
+# Entrada de datos
+
+a = 49
+
+b = 5
+
+c = 3
+
+# Procedimiento
+
+z = 24 / (c * 4)
+ 
+# Salida del programa
+
+print(' El resultado del programa es: ',z)
+
+# Fin del programa
+
+input(' pulse cualquier tecla para continuar...')# pausa
